53d37143cc7f96a5e4c19de0ce7938c7ca388850
Pau <103753396+pau-develop@users.noreply.github.com>
2022-07-14 02:20:54 +0200

    Solved infinite loop in calculator.js and refactored

--- public/ex1/calculator.js
+++ public/ex1/calculator.js
@@ -1,44 +1,33 @@
-let runLoop = true;
 let userInput;
 const inputArr = [];
+let endProgram;
 
-export function changeStr() {
+const isNotNaN = (input) => {
+  const scopeInput = parseInt(input, 10);
+  return !Number.isNaN(scopeInput);
+};
+const isInputNull = (input) => input === null;
+
+const getOperationLength = (array) => array.length;
+
+function changeStr(operationLength) {
   let string;
-  if (inputArr.length === 0) {
+  if (operationLength.length === 0) {
     if (userInput === undefined) string = `Dame un número`;
     else if (userInput === null)
       string = `Me tienes que dar un número por lo menos ¬¬`;
-  } else if (inputArr.length === 1 && userInput !== null)
+  } else if (operationLength.length === 1 && userInput !== null)
     string = `Dame otro número o cancela`;
   return string;
 }
-
-export function checkCondition() {
-  if (inputArr.length === 2) runLoop = false;
-  else if (inputArr.length === 1 && userInput === null) runLoop = false;
+function showSquareRoot() {
+  alert(
+    `la raíz cuadrada de ${inputArr[0]} es ${Math.sqrt(inputArr[0]).toFixed(3)}`
+  );
 }
 
-export function getInput() {
-  const string = changeStr();
-  userInput = prompt(string);
-  if (userInput !== null) {
-    userInput = parseInt(userInput, 10);
-    if (!Number.isNaN(userInput)) inputArr.push(userInput);
-  }
-  checkCondition();
-}
-
-export function showResults() {
-  if (inputArr.length === 1)
-    alert(
-      `la raíz cuadrada de ${inputArr[0]} es ${Math.sqrt(inputArr[0]).toFixed(
-        3
-      )}`
-    );
-  else
-    alert(`El resultado de la suma de tus números es ${
-      inputArr[0] + inputArr[1]
-    }
+function showMathOps() {
+  alert(`El resultado de la suma de tus números es ${inputArr[0] + inputArr[1]}
   El resultado de la resta de tus números es ${inputArr[0] - inputArr[1]}
   El resultado de la multiplicación de tus números es ${
     inputArr[0] * inputArr[1]
@@ -46,8 +35,23 @@ export function showResults() {
   El resultado de la división de tus números es ${inputArr[0] / inputArr[1]}`);
 }
 
-while (runLoop) {
-  getInput();
+function getInput() {
+  const myStr = changeStr(inputArr);
+  userInput = prompt(myStr);
+  const isNull = isInputNull(userInput);
+  const isNumber = isNotNaN(userInput);
+  const operationLength = getOperationLength(inputArr);
+  if (isNull && operationLength === 1) {
+    showSquareRoot();
+    endProgram = true;
+  } else if (!isNull && isNumber) {
+    inputArr.push(userInput);
+    if (operationLength === 1) {
+      showMathOps();
+      endProgram = true;
+    }
+  }
+  return endProgram ? 0 : getInput();
 }
 
-if (!runLoop) showResults();
+if (!endProgram) getInput();
